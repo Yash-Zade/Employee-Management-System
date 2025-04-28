211cb00a5b1573d2761df17baa7e1bc5ba6aec5e
Yash Zade <yashmzade@gmail.com>
2025-04-28 10:26:59 +0530

    Add create task page for admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Login from './components/Auth/login'
 import EmployeeDashboard from './components/Dashboards/EmployeeDashboard'
+import AdminDashboard from './components/Dashboards/AdminDashboard'
 
 const App = () => {
   return (
     <>
       {/* <Login /> */}
-      <EmployeeDashboard />
+      {/* <EmployeeDashboard /> */}
+      <AdminDashboard />
     </>
   )
 }
---
+++ src/components/Dashboards/AdminDashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Others/Header'
+import CreateTask from '../Others/CreateTask'
+
+const AdminDashboard = () => {
+  return (
+    <div>
+        <Header />
+        <CreateTask />
+    </div>
+  )
+}
+
+export default AdminDashboard
